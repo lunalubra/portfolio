63249e4852938b38113b4fdec197915dff4a2b97
Lucas Acosta <lucas2002lucas@gmail.com>
2021-05-18 16:39:12 +0200

    Changed blue to gray

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,8 @@ const NavLink = ({ children }) => (
     py={1}
     href={`#${children.toLowerCase()}`}
     rounded={'md'}
-    bg="blue.700"
+    bg="gray.50"
+    color="gray.900"
     _hover={{
       textDecoration: 'none',
       bg: 'gray.200'
