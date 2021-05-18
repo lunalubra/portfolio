80e8ce4dfdb9f07db55c7e5e3a4ed978b1b5620c
Lucas Acosta <lucas2002lucas@gmail.com>
2021-05-18 15:48:45 +0200

    .

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,6 @@
+import { ColorModeScript } from '@chakra-ui/color-mode';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import theme from '../styles/theme';
 
 class MyDocument extends Document {
   render() {
@@ -17,6 +19,7 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
