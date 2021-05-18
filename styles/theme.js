a0db3719821e7a34b317b33a205468c4fa9ef9cf
Lucas Acosta <lucas2002lucas@gmail.com>
2021-05-18 16:04:23 +0200

    .

--- styles/theme.js
+++ styles/theme.js
@@ -1,10 +1,6 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  config: {
-    initialColorMode: 'light',
-    useSystemColorMode: false
-  },
   fonts: {
     heading: 'Work Sans',
     body: 'Work Sans'
